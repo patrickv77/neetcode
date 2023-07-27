6e7d0dc56924ecadca3b0aa6e7881473616a3b7a
Patrick Vuong <patrickv77@gmail.com>
2023-07-27 10:12:20 -0500

    first solution utilizing two pointers

--- TwoPointers/easy/validPalindome.js
+++ TwoPointers/easy/validPalindome.js
@@ -6,6 +6,13 @@
  * 1 <= s.length <= 2 * 105
  * s consists only of printable ASCII characters.
  */
+
+
 var isPalindrome = function(s) {
-  
+  // remove all non alpha-numeric chars
+  const replaced = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  for(let i = 0, j = replaced.length-1; i <= replaced.length/2, j >= replaced.length/2; i++, j--){
+    if(replaced[i] !== replaced[j]) return false;
+  }
+  return true;
 };
