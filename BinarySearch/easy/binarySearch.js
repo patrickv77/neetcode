165f6dc11cfb7da5674349d8d7cb2383954c96a1
Patrick Vuong <patrickv77@gmail.com>
2023-07-28 09:46:32 -0500

    binarySearch boilerplate

---
+++ BinarySearch/easy/binarySearch.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ * 
+ * Constraints:
+ * 
+ * 1 <= nums.length <= 104
+ * -104 < nums[i], target < 104
+ * All the integers in nums are unique.
+ * nums is sorted in ascending order.
+ */
+var search = function(nums, target) {
+    
+};
