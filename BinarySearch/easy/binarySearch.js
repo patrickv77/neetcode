3366fc7f5e14f5b61da3cf1b25588fb920137eab
Patrick Vuong <patrickv77@gmail.com>
2023-07-28 10:55:11 -0500

    refactor with slight optimization to algo

--- BinarySearch/easy/binarySearch.js
+++ BinarySearch/easy/binarySearch.js
@@ -14,24 +14,27 @@
 // runtime 69ms -- beats 31.69%
 // memory 45.16mb -- beats 44.92%
 
+// change bounds to be index inclusive and while condition
+// runtime 63ms -- beats 63.17%
+// memory 45.16mb -- beats 44.92%
+
 // MUST BE O(logn) time!! cannot use indexof or a for loop, that is O(n).
 var search = function (nums, target) {
-  // lower and upper are 1 removed from actual indexes so the algorithm can see the actual 0 and nums.length - 1 indexes
-  let lower = -1,
-    upper = nums.length;
+  let lower = 0,
+    upper = nums.length - 1;
   let mid = Math.floor(nums.length / 2);
-  while (upper - lower !== 1) {
+  while (lower <= upper) {
     if (nums[mid] === target) return mid;
     else if (nums[mid] > target) {
-      upper = mid;
+      upper = mid - 1;
+      mid = Math.floor(lower + (upper - lower) / 2);
     } else {
-      lower = mid;
+      lower = mid + 1;
+      mid = Math.floor(lower + (upper - lower) / 2);
     }
-
-    mid = Math.floor(lower + (upper - lower) / 2);
   }
 
   return -1;
 };
 
-console.log(search([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11));
+console.log(search([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], -90));
