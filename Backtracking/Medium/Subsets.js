d95c6d596a55f64da230d81c025f26b32f3ad351
Patrick Vuong <patrickv77@gmail.com>
2023-08-01 10:40:45 -0500

    subsets solution

--- Backtracking/Medium/Subsets.js
+++ Backtracking/Medium/Subsets.js
@@ -8,5 +8,21 @@
  * All the numbers of nums are unique.
  */
 var subsets = function(nums) {
-    
+    let res = [];
+    const subsetsHelper = (n, arr) => {
+      if(n === nums.length) {
+        res.push(arr);
+        return;
+      }
+
+      // take it
+      subsetsHelper(n+1, [...arr, nums[n]]);
+      // leave it
+      subsetsHelper(n+1, [...arr]);
+    }
+
+    subsetsHelper(0,[]);
+    return res;
 };
+
+console.log(subsets([1,2,3]));
