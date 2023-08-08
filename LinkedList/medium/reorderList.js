61c7d27a12565ac7a663b56a6da36349a470a11d
Patrick Vuong <patrickv77@gmail.com>
2023-08-08 09:27:00 -0500

    file directory for linkedlist, reorderlist boilerplate

---
+++ LinkedList/medium/reorderList.js
@@ -0,0 +1,11 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
