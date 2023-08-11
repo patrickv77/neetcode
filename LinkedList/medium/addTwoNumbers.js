3478012942139f9d06d20b7de5dd357207948122
Patrick Vuong <patrickv77@gmail.com>
2023-08-11 09:38:38 -0500

    addTwoNumbers boilerplate

---
+++ LinkedList/medium/addTwoNumbers.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ *  
+ * Constraints:
+
+  The number of nodes in each linked list is in the range [1, 100].
+  0 <= Node.val <= 9
+  It is guaranteed that the list represents a number that does not have leading zeros.
+ */
+var addTwoNumbers = function(l1, l2) {
+    
+};
