e218cca557253a73c12db45c7b619a21473c802a
Patrick Vuong <patrickv77@gmail.com>
2023-08-15 10:35:31 -0500

    tests passing, stats added

--- LinkedList/easy/linkedListCycle.js
+++ LinkedList/easy/linkedListCycle.js
@@ -1,10 +1,9 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
+
+// Definition for singly-linked list.
+function ListNode(val, next) {
+  this.val = (val===undefined ? 0 : val)
+  this.next = (next===undefined ? null : next)
+}
 
 /**
  * @param {ListNode} head
@@ -16,6 +15,27 @@
   -105 <= Node.val <= 105
   pos is -1 or a valid index in the linked-list.
  */
+
+// runtime 66ms -- beats 73.30%
+// memory 44.56mb -- beats 87.51%
 var hasCycle = function(head) {
-    
+  if(head === null) return false;
+
+  let slow = head;
+  let fast = head;
+  while(fast.next !== null && fast.next.next !== null){
+    fast = fast.next.next;
+    slow = slow.next;
+    if(slow === fast) return true;
+  }
+  return false;
 };
+
+// const five = new ListNode(5);
+// const four = new ListNode(4, five);
+// const three = new ListNode(3, four);
+// const two = new ListNode(2, three);
+const one = new ListNode(1);
+one.next = one;
+
+console.log(hasCycle(one));
