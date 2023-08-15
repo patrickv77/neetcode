ab92d6bd7391b7f8f6ad4acbfb298909d4489fa3
Patrick Vuong <patrickv77@gmail.com>
2023-08-15 10:16:41 -0500

    easy folder for linked list, linkedlistcycle boilerplate

---
+++ LinkedList/easy/linkedListCycle.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ * 
+ * Constraints:
+
+  The number of the nodes in the list is in the range [0, 104].
+  -105 <= Node.val <= 105
+  pos is -1 or a valid index in the linked-list.
+ */
+var hasCycle = function(head) {
+    
+};
