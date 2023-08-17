4c771a589951c3b52a97ef2f0a1ae0e836b136af
Patrick Vuong <patrickv77@gmail.com>
2023-08-17 10:13:17 -0500

    first pass solution

--- LinkedList/medium/findDuplicateNumber.js
+++ LinkedList/medium/findDuplicateNumber.js
@@ -3,5 +3,13 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {  
-  
+  let max = -nums.length;
+  let sumNums = 0;
+  for(let i = 0; i < nums.length; i++) {
+    max += i + 1;
+    sumNums += nums[i];
+  }
+  return sumNums - max;
 };
+
+// console.log(findDuplicate([1,2,3,4,5,3]));
