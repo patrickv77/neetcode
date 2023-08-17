45aab9814e64d27aa76eb021a2b7b751b3bf73ed
Patrick Vuong <patrickv77@gmail.com>
2023-08-17 14:33:15 -0500

    floyd's cycle detection solution

--- LinkedList/medium/findDuplicateNumber.js
+++ LinkedList/medium/findDuplicateNumber.js
@@ -3,13 +3,19 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {  
-  let max = -nums.length;
-  let sumNums = 0;
-  for(let i = 0; i < nums.length; i++) {
-    max += i + 1;
-    sumNums += nums[i];
-  }
-  return sumNums - max;
+  let slow = 0, fast = 0;
+  do{
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  }while(slow !== fast)
+
+  let slow2 = 0;
+  do{
+    slow = nums[slow]
+    slow2 = nums[slow2];
+  }while(slow !== slow2)
+
+  return slow;
 };
 
-// console.log(findDuplicate([1,2,3,4,5,3]));
+console.log(findDuplicate([2,2,2,2,2]));
