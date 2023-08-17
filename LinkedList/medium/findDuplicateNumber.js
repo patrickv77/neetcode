51aa6ee0eef7efbd9cc490ea84e6d368f21f1510
Patrick Vuong <patrickv77@gmail.com>
2023-08-17 14:35:24 -0500

    stats and linting

--- LinkedList/medium/findDuplicateNumber.js
+++ LinkedList/medium/findDuplicateNumber.js
@@ -2,20 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findDuplicate = function(nums) {  
-  let slow = 0, fast = 0;
-  do{
+
+// runtime 66ms -- beats 73.30%
+// memory 44.56mb -- beats 87.51%
+var findDuplicate = function (nums) {
+  let slow = 0,
+    fast = 0;
+  do {
     slow = nums[slow];
     fast = nums[nums[fast]];
-  }while(slow !== fast)
+  } while (slow !== fast);
 
   let slow2 = 0;
-  do{
-    slow = nums[slow]
+  do {
+    slow = nums[slow];
     slow2 = nums[slow2];
-  }while(slow !== slow2)
+  } while (slow !== slow2);
 
   return slow;
 };
 
-console.log(findDuplicate([2,2,2,2,2]));
+console.log(findDuplicate([2, 2, 2, 2, 2]));
