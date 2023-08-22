254f76893b71f8a06f11547c02ad8cc197f12825
Patrick Vuong <patrickv77@gmail.com>
2023-08-22 10:35:14 -0500

    tests passing, add stats

--- LinkedList/easy/reverseLinkedList.js
+++ LinkedList/easy/reverseLinkedList.js
@@ -8,19 +8,30 @@ function ListNode(val, next) {
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function(head) {
-  if(!head) return null;
+// runtime 63ms -- beats 41.67%
+// memory 43.29mb -- beats 99.18%
+var reverseList = function (head) {
+  if (!head) return null;
 
   let prev = null;
   let curr = head;
   let next = curr.next;
 
-  while(next) {
+  while (next) {
     curr.next = prev;
     prev = curr;
     curr = next;
     next = next.next;
   }
+
   curr.next = prev;
   return curr;
 };
+
+// const five = new ListNode(5);
+// const four = new ListNode(4, five);
+// const three = new ListNode(3, four);
+// const two = new ListNode(2, three);
+// const one = new ListNode(1, two);
+
+// console.log(reverseList(one));
