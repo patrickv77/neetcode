57e804e84babe35c231609052919c961975690b5
Patrick Vuong <patrickv77@gmail.com>
2023-08-22 10:02:38 -0500

    reverseLinkedList boilerplate

---
+++ LinkedList/easy/reverseLinkedList.js
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  // store next as temp, set curr.next to null or prev
+  
+};
