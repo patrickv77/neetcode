d8b0c259adcaef49d4999f749f9fb668b4ae378c
Patrick Vuong <patrickv77@gmail.com>
2023-08-22 10:03:04 -0500

    list node constructor

--- LinkedList/easy/reverseLinkedList.js
+++ LinkedList/easy/reverseLinkedList.js
@@ -1,15 +1,14 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
+// Definition for singly-linked list.
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
 var reverseList = function(head) {
   // store next as temp, set curr.next to null or prev
-  
+
 };
