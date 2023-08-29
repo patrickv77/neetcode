0586e9b43b8795d6f44af10607da9bd410efd698
Patrick Vuong <patrickv77@gmail.com>
2023-08-29 13:43:06 -0500

    stats and fix 1-indexing answer

--- TwoPointers/medium/twoSumII.js
+++ TwoPointers/medium/twoSumII.js
@@ -3,26 +3,26 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(numbers, target) {
+
+// runtime 52ms -- beats 85.81%
+// memory 42.98mb -- beats 40.00%
+var twoSum = function (numbers, target) {
   // ALWAYS a solution ie dont worry about numbers being empty
   // left and right pointers, if less, move left pointer, if greater, move right pointer
-
-  // graciously 1-indexed... wonderful
   let left = 0;
-  let right = numbers.length-1;
+  let right = numbers.length - 1;
 
-  while((numbers[left] + numbers[right]) !== target) {
-    if((numbers[left] + numbers[right]) > target) {
+  while (numbers[left] + numbers[right] !== target) {
+    if (numbers[left] + numbers[right] > target) {
       right--;
-    }else if((numbers[left] + numbers[right]) < target) {
+    } else if (numbers[left] + numbers[right] < target) {
       left++;
     }
   }
 
-  return [left,right];
+  return [left + 1, right + 1];
 };
 
 // console.log(twoSum([2,7,11,15], 9));
 // console.log(twoSum([2,3,4],6));
 // console.log(twoSum([-1,0,1,2],-1))
-
