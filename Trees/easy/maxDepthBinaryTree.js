5b66fab56eddcc35026ed09283154a67878bf087
Patrick Vuong <patrickv77@gmail.com>
2023-08-31 11:23:53 -0500

    first submission solution

--- Trees/easy/maxDepthBinaryTree.js
+++ Trees/easy/maxDepthBinaryTree.js
@@ -10,6 +10,29 @@ function TreeNode(val, left, right) {
  * @return {number}
  */
 var maxDepth = function(root) {
-    
+  if(root === null) return 0;
+
+  let countDepth = (root, count) => {
+    if(root === null) return count;
+    else {
+      return Math.max(countDepth(root.left, count+1), countDepth(root.right, count+1));
+    }
+  }
+
+  return Math.max(countDepth(root.left,1), countDepth(root.right,1));
 };
 
+// let three = new TreeNode(3);
+// let nine = new TreeNode(9);
+// let twenty = new TreeNode(20);
+// let fifteen = new TreeNode(15);
+// let seven = new TreeNode(7);
+// let eight = new TreeNode(8);
+
+// three.left = nine;
+// three.right = twenty;
+// twenty.left = fifteen;
+// twenty.right = seven;
+// seven.left = eight;
+
+console.log(maxDepth(null));
