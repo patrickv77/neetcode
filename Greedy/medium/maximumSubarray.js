a2f1f7b4526584d5f4ffa9154b3cff2ec9bbc911
Patrick Vuong <patrickv77@gmail.com>
2023-09-05 14:09:36 -0500

    tests passing, stats, linting

--- Greedy/medium/maximumSubarray.js
+++ Greedy/medium/maximumSubarray.js
@@ -2,19 +2,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
-  let max = nums[0]
+
+// runtime 71ms -- beats 66.53%
+// memory 50.1mb -- beats 69.60%
+var maxSubArray = function (nums) {
+  let max = nums[0];
   let acc = 0;
-  for(let i = 0; i < nums.length; i++) {
+  for (let i = 0; i < nums.length; i++) {
     acc += nums[i];
-    if(acc > max) max = acc;
-    if(acc <= 0) acc = 0;
-  } 
+    if (acc > max) max = acc;
+    if (acc <= 0) acc = 0;
+  }
   return max;
 };
 
 // const arr = [-2,1,-3,4,-1,2,1,-5,4];
 // const arr = [1]
-const arr = [5,4,-1,7,8]
+const arr = [5, 4, -1, 7, 8];
 
 console.log(maxSubArray(arr));
