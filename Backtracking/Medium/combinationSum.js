e23b42e7f7cf9a37dd60e0f7086f4f01342eb697
Patrick Vuong <patrickv77@gmail.com>
2023-09-13 11:42:28 -0500

    all tests passing, stats, linting

--- Backtracking/Medium/combinationSum.js
+++ Backtracking/Medium/combinationSum.js
@@ -3,23 +3,30 @@
  * @param {number} target
  * @return {number[][]}
  */
-var combinationSum = function(candidates, target) {
+
+// runtime 71ms -- beats 50.74%
+// memory 49.33mb -- beats 10.41%
+var combinationSum = function (candidates, target) {
   let res = [];
   const combinationSumHelper = (candidatesArray, curr, acc) => {
-    if(acc === 0) res.push(curr)
-    else{
-      for(let i = 0; i < candidatesArray.length; i++) {
+    if (acc === 0) res.push(curr);
+    else {
+      for (let i = 0; i < candidatesArray.length; i++) {
         let tempArr = [...curr];
-        if(acc - candidatesArray[i] >= 0) {
+        if (acc - candidatesArray[i] >= 0) {
           tempArr.push(candidatesArray[i]);
-          combinationSumHelper(candidatesArray.slice(i), tempArr, acc - candidatesArray[i]);
+          combinationSumHelper(
+            candidatesArray.slice(i),
+            tempArr,
+            acc - candidatesArray[i]
+          );
         }
       }
     }
-  }
+  };
 
-  combinationSumHelper(candidates,[],target)
+  combinationSumHelper(candidates, [], target);
   return res;
 };
 
-console.log(combinationSum([2,3,6,7], 7))
+// console.log(combinationSum([2, 3, 6, 7], 7));
