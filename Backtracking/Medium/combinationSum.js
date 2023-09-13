31e1510b53cc28e1109b86ab62444c8950c36508
Patrick Vuong <patrickv77@gmail.com>
2023-09-13 11:41:30 -0500

    first pass solution

--- Backtracking/Medium/combinationSum.js
+++ Backtracking/Medium/combinationSum.js
@@ -4,5 +4,22 @@
  * @return {number[][]}
  */
 var combinationSum = function(candidates, target) {
-    
+  let res = [];
+  const combinationSumHelper = (candidatesArray, curr, acc) => {
+    if(acc === 0) res.push(curr)
+    else{
+      for(let i = 0; i < candidatesArray.length; i++) {
+        let tempArr = [...curr];
+        if(acc - candidatesArray[i] >= 0) {
+          tempArr.push(candidatesArray[i]);
+          combinationSumHelper(candidatesArray.slice(i), tempArr, acc - candidatesArray[i]);
+        }
+      }
+    }
+  }
+
+  combinationSumHelper(candidates,[],target)
+  return res;
 };
+
+console.log(combinationSum([2,3,6,7], 7))
