71da8dc1cb46111bbdca8a5503c15f48b0774fb1
Patrick Vuong <patrickv77@gmail.com>
2023-09-18 11:04:22 -0500

    all tests passing, stats, linting, comments

--- Backtracking/Medium/permutations.js
+++ Backtracking/Medium/permutations.js
@@ -2,22 +2,37 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+
+// runtime 57ms -- beats 84.18%
+// memory 44.99mb -- beats 44.88%
 var permute = function (nums) {
+  // declare array to return stored permutations
   let res = [];
+
+  // helper function that handles the backtracking
   const permuteHelper = (num, numsArr, currArr) => {
     if (numsArr.length === 0) {
+      // when numsArr has no numbers left, push permutation array into res
       res.push([...currArr, num]);
       return;
-    }else{
-      for(let i = 0; i < numsArr.length; i++) {
-        permuteHelper(numsArr[i], [...numsArr.slice(0,i), ...numsArr.slice(i+1)], [...currArr, num]);
+    } else {
+      // loop through nums arr and create a new recursive chain
+      for (let i = 0; i < numsArr.length; i++) {
+        permuteHelper(
+          numsArr[i],
+          [...numsArr.slice(0, i), ...numsArr.slice(i + 1)],
+          [...currArr, num]
+        );
       }
     }
   };
 
-  for(let i = 0; i < nums.length; i++) {
-    permuteHelper(nums[i], [...nums.slice(0,i), ...nums.slice(i+1)], []);
+  // loop through nums and start building permutations recursively
+  for (let i = 0; i < nums.length; i++) {
+    permuteHelper(nums[i], [...nums.slice(0, i), ...nums.slice(i + 1)], []);
   }
+
+  // return res
   return res;
 };
 
