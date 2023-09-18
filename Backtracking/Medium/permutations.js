264f200081b6729db67db6da4c2292b0617a5f39
Patrick Vuong <patrickv77@gmail.com>
2023-09-18 11:00:41 -0500

    first pass solution

--- Backtracking/Medium/permutations.js
+++ Backtracking/Medium/permutations.js
@@ -2,6 +2,23 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permute = function(nums) {
-    
+var permute = function (nums) {
+  let res = [];
+  const permuteHelper = (num, numsArr, currArr) => {
+    if (numsArr.length === 0) {
+      res.push([...currArr, num]);
+      return;
+    }else{
+      for(let i = 0; i < numsArr.length; i++) {
+        permuteHelper(numsArr[i], [...numsArr.slice(0,i), ...numsArr.slice(i+1)], [...currArr, num]);
+      }
+    }
+  };
+
+  for(let i = 0; i < nums.length; i++) {
+    permuteHelper(nums[i], [...nums.slice(0,i), ...nums.slice(i+1)], []);
+  }
+  return res;
 };
+
+// console.log(permute([0,1]))
