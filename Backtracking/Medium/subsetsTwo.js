2a46003b02c8840cbc0ee825507d6a3aefdf4644
Patrick Vuong <patrickv77@gmail.com>
2023-09-19 10:54:31 -0500

    first pass solution

--- Backtracking/Medium/subsetsTwo.js
+++ Backtracking/Medium/subsetsTwo.js
@@ -3,5 +3,21 @@
  * @return {number[][]}
  */
 var subsetsWithDup = function(nums) {
-    
+  let res = {};
+  const subsetsHelper = (n, arr) => {
+    if(n === nums.length) {
+      if(!res[arr]) res[arr] = arr;
+      return;
+    }
+
+    // take it
+    subsetsHelper(n+1, [...arr, nums[n]]);
+    // leave it
+    subsetsHelper(n+1, [...arr]);
+  }
+
+  subsetsHelper(0,[]);
+  return Object.values(res);
 };
+
+console.log(subsetsWithDup([1,2,2]));
