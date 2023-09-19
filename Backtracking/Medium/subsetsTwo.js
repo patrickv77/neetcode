ae2622af0f91605cc82bb139125ab3eec9dde047
Patrick Vuong <patrickv77@gmail.com>
2023-09-19 10:58:50 -0500

    all tests passing, stats, linting

--- Backtracking/Medium/subsetsTwo.js
+++ Backtracking/Medium/subsetsTwo.js
@@ -2,23 +2,26 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var subsetsWithDup = function(nums) {
+
+// runtime 67ms -- beats 26.59%
+// memory 45.28mb -- beats 14.30%
+var subsetsWithDup = function (nums) {
   let res = {};
   const subsetsHelper = (n, arr) => {
-    if(n === nums.length) {
-      let sort = arr.sort((a,b) => a - b);
-      if(!res[sort]) res[sort] = sort;
+    if (n === nums.length) {
+      let sort = arr.sort((a, b) => a - b);
+      if (!res[sort]) res[sort] = sort;
       return;
     }
 
     // take it
-    subsetsHelper(n+1, [...arr, nums[n]]);
+    subsetsHelper(n + 1, [...arr, nums[n]]);
     // leave it
-    subsetsHelper(n+1, [...arr]);
-  }
+    subsetsHelper(n + 1, [...arr]);
+  };
 
-  subsetsHelper(0,[]);
+  subsetsHelper(0, []);
   return Object.values(res);
 };
 
-console.log(subsetsWithDup([4,4,4,1,4]));
+console.log(subsetsWithDup([4, 4, 4, 1, 4]));
