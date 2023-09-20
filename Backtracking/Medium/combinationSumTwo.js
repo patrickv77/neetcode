ab922e68dae38c5829d356233e0710d1e271a412
Patrick Vuong <patrickv77@gmail.com>
2023-09-20 12:05:54 -0500

    first pass solution

--- Backtracking/Medium/combinationSumTwo.js
+++ Backtracking/Medium/combinationSumTwo.js
@@ -3,6 +3,37 @@
  * @param {number} target
  * @return {number[][]}
  */
-var combinationSum2 = function(candidates, target) {
-    
+var combinationSum2 = function (candidates, target) {
+  let res = {};
+  const combinationHelper = (array, acc, remainingCandidates) => {
+    if (target - acc === 0) {
+      array = array.sort((a, b) => a - b);
+      if (!res[array]) res[array] = array;
+      return;
+    } else if (target - acc < 0) {
+      return;
+    } else {
+      for (let i = 0; i < remainingCandidates.length; i++) {
+        combinationHelper(
+          [...array, remainingCandidates[i]],
+          acc + remainingCandidates[i],
+          [
+            ...remainingCandidates.slice(0, i),
+            ...remainingCandidates.slice(i + 1),
+          ]
+        );
+      }
+    }
+  };
+
+  for (let i = 0; i < candidates.length; i++) {
+    combinationHelper([candidates[i]], candidates[i], [
+      ...candidates.slice(0, i),
+      ...candidates.slice(i + 1),
+    ]);
+  }
+
+  return Object.values(res);
 };
+
+console.log(combinationSum2([10,1,2,7,6,1,5], 8))
