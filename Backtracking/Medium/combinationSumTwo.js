35071e19ca0cb5f7dfa94d34c38a51156f05bf3f
Patrick Vuong <patrickv77@gmail.com>
2023-09-22 11:23:48 -0500

    add comment on line 42

--- Backtracking/Medium/combinationSumTwo.js
+++ Backtracking/Medium/combinationSumTwo.js
@@ -39,6 +39,12 @@ var combinationSum2 = function (candidates, target) {
   return Object.values(res);
 };
 
-console.log(combinationSum2([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], 27))
+// new concept, in each recursive step pass down the new array and target - sum of array as new target, remove any candidates taht are higher than new target
+
+
+console.log(combinationSum2([1,100,101,1012,1015,1011,10155,1231231,12222,15555,177777,222222,3333,888888], 1));
+// console.log(combinationSum2([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], 27))
 // console.log(combinationSum2([1,1,1,1,1,1,1,1,1],15));
 // [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+// console.log(combinationSum2([29,19,14,33,11,5,9,23,23,33,12,9,25,25,12,21,14,11,20,30,17,19,5,6,6,5,5,11,12,25,31,28,31,33,27,7,33,31,17,13,21,24,17,12,6,16,20,16,22,5], 28));
