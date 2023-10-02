a5e0a03c940c4473190a86d5473a32186b2546c3
Patrick Vuong <patrickv77@gmail.com>
2023-10-02 15:56:36 -0500

    linting, comments combination sum 2

--- Backtracking/Medium/combinationSumTwo.js
+++ Backtracking/Medium/combinationSumTwo.js
@@ -4,9 +4,12 @@
  * @return {number[][]}
  */
 var combinationSum2 = function (candidates, target) {
-  if(candidates.reduce((acc, x) => acc + x, 0) < target) return [];
+  // if sum of candidates is less than target, return empty array
+  if (candidates.reduce((acc, x) => acc + x, 0) < target) return [];
 
+  // otherwise declare empty object res to hold arrays
   let res = {};
+  // helper function
   const combinationHelper = (array, acc, remainingCandidates) => {
     console.log(remainingCandidates, acc);
     if (target - acc === 0) {
@@ -29,6 +32,7 @@ var combinationSum2 = function (candidates, target) {
     }
   };
 
+  // invoke helper function
   for (let i = 0; i < candidates.length; i++) {
     combinationHelper([candidates[i]], candidates[i], [
       ...candidates.slice(0, i),
@@ -36,13 +40,21 @@ var combinationSum2 = function (candidates, target) {
     ]);
   }
 
+  // return values
   return Object.values(res);
 };
 
 // new concept, in each recursive step pass down the new array and target - sum of array as new target, remove any candidates taht are higher than new target
 
-
-console.log(combinationSum2([1,100,101,1012,1015,1011,10155,1231231,12222,15555,177777,222222,3333,888888], 1));
+console.log(
+  combinationSum2(
+    [
+      1, 100, 101, 1012, 1015, 1011, 10155, 1231231, 12222, 15555, 177777,
+      222222, 3333, 888888,
+    ],
+    1
+  )
+);
 // console.log(combinationSum2([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], 27))
 // console.log(combinationSum2([1,1,1,1,1,1,1,1,1],15));
 // [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
