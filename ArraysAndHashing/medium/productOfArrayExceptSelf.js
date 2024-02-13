9d9a522ebb94b609e353cc341830a4d9ce5bb009
Patrick Vuong <patrickv77@gmail.com>
2024-02-13 10:59:33 -0600

    first pass solution

--- ArraysAndHashing/medium/productOfArrayExceptSelf.js
+++ ArraysAndHashing/medium/productOfArrayExceptSelf.js
@@ -9,5 +9,24 @@
  * 
  */
 var productExceptSelf = function(nums) {
+    // get result in two passes, first pass will append to an array the product of the numbers left of i
+    // second pass will multiply the numbers to the right of i by the previous results
+
+    let res = [];
+
+    let prefix = 1;
+
+    for(let i = 0; i < nums.length; i++) {
+      res[i] = prefix;
+      prefix *= nums[i];
+    }
+
+    let suffix = 1;
     
+    for(let j = nums.length-1; j >= 0; j--) {
+      res[j] *= suffix;
+      suffix *= nums[j];
+    }
+
+    return res;
 };
