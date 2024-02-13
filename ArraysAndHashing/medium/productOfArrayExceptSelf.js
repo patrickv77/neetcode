2e54fd71475907accbbda1a96456b910e92827e4
Patrick Vuong <patrickv77@gmail.com>
2024-02-13 11:03:52 -0600

    add comments for clarity

--- ArraysAndHashing/medium/productOfArrayExceptSelf.js
+++ ArraysAndHashing/medium/productOfArrayExceptSelf.js
@@ -12,21 +12,29 @@ var productExceptSelf = function(nums) {
     // get result in two passes, first pass will append to an array the product of the numbers left of i
     // second pass will multiply the numbers to the right of i by the previous results
 
+    // declare result array
     let res = [];
 
+    // declare accumulator of product of left values as prefix
     let prefix = 1;
 
+    // first pass, assign res[i] to the prefix
+    // update prefix at each step
     for(let i = 0; i < nums.length; i++) {
       res[i] = prefix;
       prefix *= nums[i];
     }
 
+    // declare accumulator of product of right values as suffix
     let suffix = 1;
     
+    // second pass, assign res[j] to suffix * prefix
+    // update suffix at each step
     for(let j = nums.length-1; j >= 0; j--) {
       res[j] *= suffix;
       suffix *= nums[j];
     }
 
+    // return result array
     return res;
 };
