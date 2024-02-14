e0e73f7a59bd795f67b992ad6718e432d60d0b5a
Patrick Vuong <patrickv77@gmail.com>
2024-02-14 10:48:49 -0600

    longest consecutive sequence boilerplate

---
+++ ArraysAndHashing/medium/longestConsecutiveSequence.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * 
+ * Constraints:
+ * 0 <= nums.length <= 105
+ * -109 <= nums[i] <= 109
+ * 
+ */
+var longestConsecutive = function(nums) {
+    
+};
