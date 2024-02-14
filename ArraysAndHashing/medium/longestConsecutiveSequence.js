80e0dd26f29d2c6118fec52a9346e8fe0befb759
Patrick Vuong <patrickv77@gmail.com>
2024-02-14 11:00:20 -0600

    comments

--- ArraysAndHashing/medium/longestConsecutiveSequence.js
+++ ArraysAndHashing/medium/longestConsecutiveSequence.js
@@ -1,12 +1,16 @@
 /**
  * @param {number[]} nums
  * @return {number}
- * 
+ *
  * Constraints:
  * 0 <= nums.length <= 105
  * -109 <= nums[i] <= 109
- * 
+ *
  */
-var longestConsecutive = function(nums) {
-    
+var longestConsecutive = function (nums) {
+  // declare a set and add all values of nums to it
+  // declare a res variable to track the max number of elements among consecutive numbers
+
+  // loop through the set, for each element, check to see if it has no number before it but has a number after it
+  // this guarantees that you are starting from the lowest element
 };
