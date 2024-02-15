4abea06efdaa7b733a8aab99402167e543b9dd3a
Patrick Vuong <patrickv77@gmail.com>
2024-02-15 11:08:43 -0600

    second pass solution

--- Stack/easy/validParantheses.js
+++ Stack/easy/validParantheses.js
@@ -5,45 +5,36 @@
  * Constraints:
  * 1 <= s.length <= 104
  * s consists of parentheses only '()[]{}'.
+ * Open brackets must be closed in the correct order***
  *
  */
 var isValid = function (s) {
-  // iterate through s and at each char, if open, add to stack, if closed, remove the open from the stack.
-  // if this fails, return false.
-  let parenStack = [];
-  let curlyStack = [];
-  let braceStack = [];
+  // second attempt: brackets must be closed in the same order, first attempt was for an orderless solution
+  // declare a map and map the closed parens (key) to the open parens (values)
+  const parenMap = new Map([
+    [')', '('],
+    ['}', '{'],
+    [']', '['],
+  ]);
 
-  // in constraints, "s consists of parentheses only..." means we do not have to get complicated with checking
-  // for other characters
+  // declare a stack to track valid parentheses
+  let stack = [];
 
+  // loop through s
   for (let ch of s) {
-    let temp;
-    switch (ch) {
-      case '(':
-        parenStack.push(ch);
-        break;
-      case '{':
-        curlyStack.push(ch);
-        break;
-      case '[':
-        braceStack.push(ch);
-        break;
-      case ')':
-        temp = parenStack.pop();
-        if(temp === undefined) return false;
-        break;
-      case '}':
-        temp = curlyStack.pop();
-        if(temp === undefined) return false;
-        break;
-      case ']':
-        temp = braceStack.pop();
-        if(temp === undefined) return false;
-        break;
+    // if open parentheses, add to stack
+    if (!parenMap.has(ch)) {
+      stack.push(ch);
+    } else {
+      // else it is closed parentheses, check against stack
+      // this is the first failure case, unmatching open/close parens
+      if (parenMap.get(ch) !== stack.pop()) return false;
     }
   }
-  if(parenStack.length + curlyStack.length + braceStack.length > 0) return false;
+
+  // second failure case, uneven number of open/closed parentheses
+  if(stack.length > 0) return false;
+
+  // all failure cases passed, return true
   return true;
 };
-
