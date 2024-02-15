0bce72e76c0ce55a72bcba941880912b15055594
Patrick Vuong <patrickv77@gmail.com>
2024-02-15 10:13:52 -0600

    valid parentheses boilerplate

---
+++ Stack/easy/validParantheses.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ * 
+ * Constraints:
+ * 1 <= s.length <= 104
+ * s consists of parentheses only '()[]{}'.
+ * 
+ */
+var isValid = function(s) {
+    
+};
