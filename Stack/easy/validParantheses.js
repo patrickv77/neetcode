d1bfd7a44ee236816c7daf44663ac8b5f97bf868
Patrick Vuong <patrickv77@gmail.com>
2024-02-15 10:16:08 -0600

    comments

--- Stack/easy/validParantheses.js
+++ Stack/easy/validParantheses.js
@@ -8,5 +8,6 @@
  * 
  */
 var isValid = function(s) {
-    
+  // iterate through s and at each char, if open, add to stack, if closed, remove the open from the stack.
+  // if this fails, return false.
 };
