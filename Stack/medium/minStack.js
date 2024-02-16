f841ec3eb68a6424903f80ce53620dfd188a98a1
Patrick Vuong <patrickv77@gmail.com>
2024-02-16 14:49:32 -0600

    linting and stats

--- Stack/medium/minStack.js
+++ Stack/medium/minStack.js
@@ -94,10 +94,12 @@
 // console.log(stak);
 // console.log(stak.top());
 
-
 // refactoring based on an observation i read
 // since this is a STACK problem, we do not have to worry about insertion and deletion of elements besides the latest
 // although it bothers me a bit to implement pop() using array pop(), i will be using it for simplicity
+
+// runtime 90ms -- beats 62.81%
+// memory 59.23mb -- beats 23.54%
 var MinStack = function () {
   this.min = [];
   this.stack = [];
@@ -109,9 +111,9 @@ var MinStack = function () {
  */
 MinStack.prototype.push = function (val) {
   this.stack.push(val);
-  if(this.min.length === 0) {
+  if (this.min.length === 0) {
     this.min.push(val);
-  }else{
+  } else {
     const prev = this.min[this.min.length - 1];
     this.min.push(Math.min(prev, val));
   }
@@ -121,7 +123,7 @@ MinStack.prototype.push = function (val) {
  * @return {void}
  */
 MinStack.prototype.pop = function () {
-  if(this.stack.length === 0) return;
+  if (this.stack.length === 0) return;
   this.stack.pop();
   this.min.pop();
 };
