9a3960fbfde25fabac6079fb2a6a4c0c8dd8ca47
Patrick Vuong <patrickv77@gmail.com>
2024-02-19 09:07:56 -0600

    boilerplate for evaluate reverse polish notation algo

---
+++ Stack/medium/evaluateReversePolishNotation.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ * 
+ * Constraints:
+ * 1 <= tokens.length <= 104
+ * tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
+ * 
+ */
+var evalRPN = function(tokens) {
+    
+};
