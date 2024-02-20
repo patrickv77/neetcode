efcbbf8b345695f518548f1d1e5c72ad97472abd
Patrick Vuong <patrickv77@gmail.com>
2024-02-20 10:36:07 -0600

    first pass solution

--- Stack/medium/evaluateReversePolishNotation.js
+++ Stack/medium/evaluateReversePolishNotation.js
@@ -8,10 +8,40 @@
  * 
  */
 var evalRPN = function(tokens) {
-    // add integers to the stack
-    // when an operator is encountered, pop from the stack and evaluate using the operator
-    // push the result back onto the stack
-    // continue until the end of tokens
+  // we may have to account for tokens being length 1 and not having any value (i.e. an operator as the only token)
+  if(tokens.length === 1) return tokens[0];
 
-    // return the last value in the stack
+  const operators = new Set(['+', '-', '*', '/']);
+  const tokenStack = [];
+
+  for(let token of tokens) {
+    // add integers to the stack
+    if(!operators.has(token)){
+      tokenStack.push(token);
+    }else{
+      // when an operator is encountered, pop from the stack and evaluate using the operator
+      // push the result back onto the stack
+      // continue until the end of tokens
+      let valRight = tokenStack.pop();
+      let valLeft = tokenStack.pop();
+
+      switch(token) {
+        case '+':
+          tokenStack.push(valLeft + valRight);
+          break;
+        case '-':
+          tokenStack.push(valLeft - valRight);
+          break;
+        case '*':
+          tokenStack.push(valLeft * valRight);
+          break;
+        case '/':
+          tokenStack.push(valLeft / valRight);
+          break;
+      }
+    }
+  }
+
+  // return the last value in the stack
+  return tokenStack.pop();
 };
