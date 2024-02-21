21996709b96a6204fda2a6925b7e0b913bbfff8a
Patrick Vuong <patrickv77@gmail.com>
2024-02-21 11:38:46 -0600

    all tests passing, linting, stats

--- Stack/medium/generateParentheses.js
+++ Stack/medium/generateParentheses.js
@@ -1,27 +1,29 @@
 /**
  * @param {number} n
  * @return {string[]}
- * 
+ *
  * Constraints:
  * 1 <= n <= 8
- * 
+ *
  */
 
-var generateParenthesis = function(n) {
+// runtime 54ms -- beats 60.29%
+// memory 50.70mb -- beats 15.78%
+var generateParenthesis = function (n) {
   // declare a res array to store VALID parenthesis combinations
   const res = [];
 
   // create a helper method within the main method so it has access to res array
   const backtrack = (str, open, close) => {
-    if(str.length === 2*n) {
+    if (str.length === 2 * n) {
       res.push(str);
       return;
     }
 
     // take it or leave it backtracking method
-    if(open < n) backtrack(str + '(', open+1, close);
-    if(close < open) backtrack (str + ')', open, close+1);
-  }
+    if (open < n) backtrack(str + '(', open + 1, close);
+    if (close < open) backtrack(str + ')', open, close + 1);
+  };
 
   // call the helper method
   backtrack('', 0, 0);
