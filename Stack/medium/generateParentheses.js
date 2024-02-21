3e78ac0490ec22cc65321eb1e316cb9ae33be1ed
Patrick Vuong <patrickv77@gmail.com>
2024-02-21 10:28:42 -0600

    add comments for plan to tackle algo

--- Stack/medium/generateParentheses.js
+++ Stack/medium/generateParentheses.js
@@ -7,5 +7,7 @@
  * 
  */
 var generateParenthesis = function(n) {
+  // create a seperate function to run a backtracking algorithm
+  // n is equal to the number of OPEN parens and the number of CLOSED parens... total parens = n * 2
   
 };
