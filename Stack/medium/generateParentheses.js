18690055ed07099e3090562d931fe8e06ccc2bc2
Patrick Vuong <patrickv77@gmail.com>
2024-02-21 11:36:36 -0600

    first pass solution for generate parenthesis

--- Stack/medium/generateParentheses.js
+++ Stack/medium/generateParentheses.js
@@ -6,8 +6,25 @@
  * 1 <= n <= 8
  * 
  */
+
 var generateParenthesis = function(n) {
-  // create a seperate function to run a backtracking algorithm
-  // n is equal to the number of OPEN parens and the number of CLOSED parens... total parens = n * 2
-  
+  // declare a res array to store VALID parenthesis combinations
+  const res = [];
+
+  // create a helper method within the main method so it has access to res array
+  const backtrack = (str, open, close) => {
+    if(str.length === 2*n) {
+      res.push(str);
+      return;
+    }
+
+    // take it or leave it backtracking method
+    if(open < n) backtrack(str + '(', open+1, close);
+    if(close < open) backtrack (str + ')', open, close+1);
+  }
+
+  // call the helper method
+  backtrack('', 0, 0);
+
+  return res;
 };
