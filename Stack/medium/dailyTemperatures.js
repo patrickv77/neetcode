9e5a1c633f307f267457ebec398ece0b76511487
Patrick Vuong <patrickv77@gmail.com>
2024-02-22 11:14:11 -0600

    daily temperatures boilerplate

---
+++ Stack/medium/dailyTemperatures.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ * 
+ * Constraints:
+ * 1 <= temperatures.length <= 105
+ * 30 <= temperatures[i] <= 100
+ * 
+ */
+var dailyTemperatures = function(temperatures) {
+    
+};
