5227fc000ca6d38f5390ecc6903232a187658421
Patrick Vuong <patrickv77@gmail.com>
2024-02-22 12:47:18 -0600

    comments for plan to tackle algo

--- Stack/medium/dailyTemperatures.js
+++ Stack/medium/dailyTemperatures.js
@@ -8,5 +8,17 @@
  * 
  */
 var dailyTemperatures = function(temperatures) {
-    
+  // initialize a stack to track indices
+
+  // initialize an array with the same length as temperature and set the values to 0
+
+  // loop through temperatures from the END
+
+  // pop elements from the stack while the current temperature is greater
+
+  //if the stack is not empty, calculate the difference in indices
+
+  // push the current index onto the stack
+
+  // return the result array
 };
