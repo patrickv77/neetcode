786e46719c682a0c4531dfb0d37359de5bce37ee
Patrick Vuong <patrickv77@gmail.com>
2024-02-23 11:14:16 -0600

    car fleet boilerplate

---
+++ Stack/medium/carFleet.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} target
+ * @param {number[]} position
+ * @param {number[]} speed
+ * @return {number}
+ * 
+ * Constraints:
+ * n == position.length == speed.length
+ * 1 <= n <= 105
+ * 0 < target <= 106
+ * 0 <= position[i] < target
+ * All the values of position are unique.
+ * 0 < speed[i] <= 106
+ * 
+ */
+var carFleet = function(target, position, speed) {
+    
+};
