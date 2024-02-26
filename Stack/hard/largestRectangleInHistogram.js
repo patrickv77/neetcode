9bed62eaee353a88653eae039bb592593e9acb14
Patrick Vuong <patrickv77@gmail.com>
2024-02-26 11:16:25 -0600

    largest rectangle in histogram boilerplate

---
+++ Stack/hard/largestRectangleInHistogram.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ * 
+ * Constraints:
+ * 1 <= heights.length <= 105
+ * 0 <= heights[i] <= 104
+ * 
+ */
+var largestRectangleArea = function(heights) {
+    
+};
