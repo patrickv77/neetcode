e23a74a5b34f6196fa610d16d576794372705a41
Patrick Vuong <patrickv77@gmail.com>
2024-02-29 11:02:57 -0600

    comments to tackle algo

--- TwoPointers/medium/threeSum.js
+++ TwoPointers/medium/threeSum.js
@@ -8,5 +8,12 @@
  * 
  */
 var threeSum = function(nums) {
-    
+    // three pointers
+    // first step would be to sort the array
+
+    // loop through the new array one time
+    // declare two pointers, one starting in front of the for loop, one starting from the end
+    // adjust the left and right pointers according to the sum of the three pointers
+
+    // return empty array if you make it out of the for loop
 };
