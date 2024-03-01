6abdbe86ff8d712cce9ac82f7f30d7618070909a
Patrick Vuong <patrickv77@gmail.com>
2024-03-01 10:04:55 -0600

    boilerplate for container with most water algo

---
+++ TwoPointers/medium/containerWithMostWater.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ * 
+ * Constraints:
+ * n == height.length
+ * 2 <= n <= 105
+ * 0 <= height[i] <= 104
+ * 
+ */
+var maxArea = function(height) {
+  
+};
