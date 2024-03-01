bd9c543b39e74033b2ee3fe1fef3cd8622424538
Patrick Vuong <patrickv77@gmail.com>
2024-03-01 10:05:20 -0600

    comments for plan to tackle algo

--- TwoPointers/medium/containerWithMostWater.js
+++ TwoPointers/medium/containerWithMostWater.js
@@ -9,5 +9,11 @@
  * 
  */
 var maxArea = function(height) {
-  
+  // two pointer approach
+  // one pointer starts at the front and one at the back
+  // calculate the max area based on height at each pointer and store it
+  // move the one that is lower (or move any of them once if they are equal)
+  // exit the loop when the index at the pointers is the same
+
+  // return the max area
 };
