8bb7a1dda735548d4250fcc9b52743cfe499b7f6
Patrick Vuong <patrickv77@gmail.com>
2024-03-02 10:57:27 -0600

    first try solution

--- TwoPointers/hard/trappingRainWater.js
+++ TwoPointers/hard/trappingRainWater.js
@@ -11,7 +11,23 @@
 var trap = function (height) {
   // use two pointers
   // they start at the same point
+  let res = 0;
+  let trailing = 0;
+  let forward = 0;
+  let acc = 0;
+  
   // forward pointer moves until it is greater than or equal to height of prev pointer
   // as pointer moves, add height difference of each step
   // when the forward pointer reaches a greater than or equal height, move prev pointer to forward pointer location
+  for(forward; forward <= height.length - 1; forward++) {
+    if(height[trailing] > height[forward]){
+      acc += height[trailing] - height[forward];
+    }else{
+      res += acc;
+      trailing = forward;
+      acc = 0;
+    }
+  }
+  
+  return res;
 };
