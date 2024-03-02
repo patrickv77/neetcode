36bc060ba0f6e8760da922a79d9cf6998888b7f7
Patrick Vuong <patrickv77@gmail.com>
2024-03-02 10:44:02 -0600

    add hard section for two pointers, trap rain water boilerplate

---
+++ TwoPointers/hard/trappingRainWater.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ * 
+ * Constraints:
+ * n == height.length
+ * 1 <= n <= 2 * 104
+ * 0 <= height[i] <= 105
+ * 
+ */
+var trap = function(height) {
+  
+};
