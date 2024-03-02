8064c85da781f504b1598b5bec10a81a3e145d78
Patrick Vuong <patrickv77@gmail.com>
2024-03-02 11:22:32 -0600

    second attempt

--- TwoPointers/hard/trappingRainWater.js
+++ TwoPointers/hard/trappingRainWater.js
@@ -14,20 +14,26 @@ var trap = function (height) {
   let res = 0;
   let trailing = 0;
   let forward = 0;
-  let acc = 0;
   
   // forward pointer moves until it is greater than or equal to height of prev pointer
   // as pointer moves, add height difference of each step
   // when the forward pointer reaches a greater than or equal height, move prev pointer to forward pointer location
-  for(forward; forward <= height.length - 1; forward++) {
-    if(height[trailing] > height[forward]){
-      acc += height[trailing] - height[forward];
-    }else{
-      res += acc;
-      trailing = forward;
-      acc = 0;
+  for(forward; forward <= height.length - 2; forward++) {
+    console.log(height[forward], height[forward+1]);
+    console.log("res:", res)
+    if(height[forward] > height[forward+1]) {
+      let heightMin = Math.min(height[trailing], height[forward]);
+      while(trailing < forward) {
+        console.log("height forward", height[forward]);
+        console.log("height min: ", heightMin);
+        console.log("height[trailing]: ", height[trailing]);
+        res += heightMin - height[trailing];
+        trailing++;
+      }
     }
   }
   
   return res;
 };
+
+console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1]));
