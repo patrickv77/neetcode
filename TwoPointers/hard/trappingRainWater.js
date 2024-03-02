91e0cf5edc08aa27d7cd20c87ace21749064752a
Patrick Vuong <patrickv77@gmail.com>
2024-03-02 10:50:44 -0600

    comments to tackle algo

--- TwoPointers/hard/trappingRainWater.js
+++ TwoPointers/hard/trappingRainWater.js
@@ -1,13 +1,17 @@
 /**
  * @param {number[]} height
  * @return {number}
- * 
+ *
  * Constraints:
  * n == height.length
  * 1 <= n <= 2 * 104
  * 0 <= height[i] <= 105
- * 
+ *
  */
-var trap = function(height) {
-  
+var trap = function (height) {
+  // use two pointers
+  // they start at the same point
+  // forward pointer moves until it is greater than or equal to height of prev pointer
+  // as pointer moves, add height difference of each step
+  // when the forward pointer reaches a greater than or equal height, move prev pointer to forward pointer location
 };
