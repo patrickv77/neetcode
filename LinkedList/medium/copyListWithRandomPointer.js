b4be512f1ae4f2d48045a91003493d66f838a889
Patrick Vuong <patrickv77@gmail.com>
2024-03-03 10:10:16 -0600

    copy list with random pointer boilerplate

---
+++ LinkedList/medium/copyListWithRandomPointer.js
@@ -0,0 +1,22 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, next, random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ * 
+ * Constraints:
+ * 0 <= n <= 1000
+ * -104 <= Node.val <= 104
+ * Node.random is null or is pointing to some node in the linked list.
+ * 
+ */
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var copyRandomList = function(head) {
+    
+};
