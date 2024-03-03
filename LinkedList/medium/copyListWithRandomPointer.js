5012aec653a43f61bc3201cd9a5adb0acf45a351
Patrick Vuong <patrickv77@gmail.com>
2024-03-03 12:26:50 -0600

    first try solution

--- LinkedList/medium/copyListWithRandomPointer.js
+++ LinkedList/medium/copyListWithRandomPointer.js
@@ -18,11 +18,41 @@
  * @return {Node}
  */
 var copyRandomList = function(head) {
-  // traverse the list twice
+  // base case, empty list
+  if(!head) return null;
 
+  // traverse the list twice
   // first pass, create new nodes using the current node's value and place in a hashmap
+  const listNodeMap = new Map();
+
+  let curr = head;
+  while(curr) {
+    listNodeMap.set(curr, new Node(curr.val));
+    curr = curr.next;
+  }
 
   // second pass, map the next and random values of each node
+  curr = head;
+  while(curr) {
+    listNodeMap.get(curr).next = listNodeMap.get(curr.next) || null;
+    listNodeMap.get(curr).random = listNodeMap.get(curr.random) || null;
+    curr = curr.next;
+  }
 
   // return new head
+  return listNodeMap.get(head);
 };
+
+function Node(val, next, random) {
+  this.val = val;
+  this.next = next;
+  this.random = random;
+};
+
+let n1 = new Node(1);
+let n2 = new Node(2, null);
+n1.next = n2;
+n1.random = n2;
+n2.random = n2;
+
+console.log(copyRandomList(n1))
