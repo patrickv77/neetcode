75e729f3474330be3c2c38409eb66baf41cdfa9f
Patrick Vuong <patrickv77@gmail.com>
2024-03-04 10:43:29 -0600

    longest repeating character replacement boilerplate

---
+++ SlidingWindow/medium/longestRepeatingCharacterReplacement.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ * 
+ * Constraints:
+ * 1 <= s.length <= 105
+ * s consists of only uppercase English letters.
+ * 0 <= k <= s.length
+ * 
+ */
+var characterReplacement = function(s, k) {
+    
+};
