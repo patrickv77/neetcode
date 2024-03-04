abb56575629d9a6eb00511b4b0f88af6bbce7a18
Patrick Vuong <patrickv77@gmail.com>
2024-03-04 11:35:34 -0600

    comments to tackle algo

--- SlidingWindow/medium/longestRepeatingCharacterReplacement.js
+++ SlidingWindow/medium/longestRepeatingCharacterReplacement.js
@@ -10,5 +10,13 @@
  * 
  */
 var characterReplacement = function(s, k) {
-    
+    // start with two pointers
+    // use an object to track the count of letters
+
+    // loop through s, add the characters to the object and track the count of each
+    // track the most used letter and measure that against all other letters (should equal k);
+    // if right index minus left index plus 1 minus most used letter is greater than k, slide the window
+    // decrement from the dictionary and increment the left pointer
+
+    // return the length of the sliding window r minus l
 };
