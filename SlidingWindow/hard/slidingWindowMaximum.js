994e8847407c23fcde665c95b5fd69479b602f7d
Patrick Vuong <patrickv77@gmail.com>
2024-03-08 15:09:05 -0600

    brief description of monotonic deque

--- SlidingWindow/hard/slidingWindowMaximum.js
+++ SlidingWindow/hard/slidingWindowMaximum.js
@@ -2,13 +2,14 @@
  * @param {number[]} nums
  * @param {number} k
  * @return {number[]}
- * 
+ *
  * Constraints:
  * 1 <= nums.length <= 105
  * -104 <= nums[i] <= 104
  * 1 <= k <= nums.length
- * 
+ *
  */
-var maxSlidingWindow = function(nums, k) {
-    
+var maxSlidingWindow = function (nums, k) {
+  // implement a monotonic deque structure: essentially run one pass
+  // if you find a new max, deque everything. otherwise add it to the stack
 };
