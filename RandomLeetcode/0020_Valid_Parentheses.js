282399f6739756c36dd4966b5be831cdd974e1fd
Patrick Vuong <patrickv77@gmail.com>
2024-07-15 10:59:40 -0500

    runtime and memory statistics

--- RandomLeetcode/0020_Valid_Parentheses.js
+++ RandomLeetcode/0020_Valid_Parentheses.js
@@ -6,6 +6,9 @@
 // Strategy: Implement a stack. Parse through the string from left to right.
 // If it is an open parentheses, add it to the stack. If it is closed, remove the rightmost element
 // to compare and check validity.
+
+// runtime 52ms -- beats 84.61%
+// memory 49.6mb -- beats 84.38%
 var isValid = function (s) {
 
   const validParens = {
