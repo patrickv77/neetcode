9fd31310aa302e8e94dcd95c13836d0bc1ca1fe3
Patrick Vuong <patrickv77@gmail.com>
2024-07-15 10:28:00 -0500

    valid parens pseudocode

--- RandomLeetcode/0020_Valid_Parentheses.js
+++ RandomLeetcode/0020_Valid_Parentheses.js
@@ -2,4 +2,12 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function (s) {};
+
+// Strategy: Implement a stack. Parse through the string from left to right.
+// If it is an open parentheses, add it to the stack. If it is closed, remove the rightmost element
+// to compare and check validity.
+var isValid = function (s) {
+
+
+
+};
